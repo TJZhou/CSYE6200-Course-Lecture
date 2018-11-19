e4a8e5471be0318ab4f3c20a923f0ca3cefde482
Tianju Zhou <zhou.tianj@husky.neu.edu>
2018-11-19 18:46:49 -0500

    upload Enumeration Demo

---
+++ src/edu/neu/csye6200/enumT/EnumTest.java
@@ -0,0 +1,66 @@
+package edu.neu.csye6200.enumT;
+
+/**
+ * @author steve
+ *
+ */
+public class EnumTest {
+
+	enum WeekDay { 
+		SUNDAY(0), MONDAY(10), TUESDAY(20), WEDNESDAY(30), THURSDAY(40), FRIDAY(50), SATURDAY(60);
+	
+		private int value;
+	
+		// constructor
+		WeekDay(int n) {
+			value = n;
+		}
+		public void setValue(int value) {
+			this.value = value;
+		}
+		public int getValue() {
+			return value;
+		}
+	};
+	
+	private WeekDay currDay = WeekDay.TUESDAY;
+	
+	//constructor
+	public EnumTest() {
+		
+		System.out.println("Current day is " + currDay + " ordinal value " + currDay.ordinal());
+		
+		WeekDay days[] = WeekDay.values();
+		for(WeekDay d : days) {
+			System.out.println("Day: " + d.name() + " ordinal " + d.ordinal() + " value " + d.getValue());
+		}
+		
+		setCurrDay(WeekDay.SATURDAY);
+		setCurrDay(WeekDay.FRIDAY);
+	}
+	
+	
+	/**
+	 * @return the currDay
+	 */
+	public WeekDay getCurrDay() {
+		return currDay;
+	}
+
+
+	/**
+	 * @param currDay the currDay to set
+	 */
+	public void setCurrDay(WeekDay currDay) {
+		this.currDay = currDay;
+		System.out.println("Current day is " + currDay + " ordinal value " + currDay.ordinal());
+	}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EnumTest et = new EnumTest();		
+	}
+}
--- src/edu/neu/csye6200/ui/MyAppUI.java
+++ src/edu/neu/csye6200/ui/MyAppUI.java
@@ -3,6 +3,13 @@ package edu.neu.csye6200.ui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.logging.Logger;
 
 import javax.swing.JButton;
@@ -15,48 +22,56 @@ import javax.swing.JPanel;
  */
 public class MyAppUI {
 
-	private Logger log = Logger.getLogger(MyAppUI.class.getName());	
+	private Logger log = Logger.getLogger(MyAppUI.class.getName());
 	private JFrame frame = null;
 	private JPanel mainPanel = null;
 	private JButton startBtn = null;
 	private JButton stopBtn = null;
-	
-	//constructor
-	public MyAppUI(){
+
+	// constructor
+	public MyAppUI() {
 		log.info("App Started");
 		initGUI();
 	}
 
-	//initialize the GUI
-	private void initGUI(){
+	// initialize the GUI
+	private void initGUI() {
 		frame = new JFrame();
 		frame.setTitle("MyAppUI");
-		frame.setSize(400, 300);	//set the size to something reasonable
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//if we press the close button, exit		
+		frame.setSize(1000, 600); // set the size to something reasonable
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // if we press the close button, exit
 		frame.setLayout(new BorderLayout());
-		frame.add(getMainPanel(), BorderLayout.CENTER);	
+		frame.add(getMainPanel(), BorderLayout.CENTER);
 		frame.setVisible(true);
 	}
-	
-	//create a panel that we'll draw into
-	public JPanel getMainPanel(){
+
+	// create a panel that we'll draw into
+	public JPanel getMainPanel() {
 		mainPanel = new JPanel();
-		mainPanel.setLayout(new FlowLayout());	//flow from left to right
-		startBtn = new JButton("Start");	//create button instances
+		mainPanel.setLayout(new FlowLayout()); // flow from left to right
+		startBtn = new JButton("Start"); // create button instances
+		startBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		stopBtn = new JButton("Stop");
-		mainPanel.add(startBtn);	//add them to the panel
+		mainPanel.add(startBtn); // add them to the panel
 		mainPanel.add(stopBtn);
 		mainPanel.setBackground(Color.black);
 		return mainPanel;
 	}
-	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	MyAppUI myApp = new MyAppUI();
-	
-	System.out.println("MyAppUI is exiting");
+		MyAppUI myApp = new MyAppUI();
+
+		System.out.println("MyAppUI is exiting");
 
 	}
 }
